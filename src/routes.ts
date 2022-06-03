f43064d7612e7d304d05f0b9017cfcc2d26a1fe4
Augusto Lamim <augustocesarlamim@gmail.com>
2022-06-02 23:53:00 -0300

    fixing cors options

--- src/routes.ts
+++ src/routes.ts
@@ -21,7 +21,7 @@ const options: cors.CorsOptions = {
     'Content-Type',
     'Accept',
     'X-Access-Token',
-    'Access-Control-Allow-Headers'
+    'Authorization'
   ],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
